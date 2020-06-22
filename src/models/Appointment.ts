e18ee3b0d1e7e64a69fc97aaa54a2a522277df30
alanalv3s <alan_ams97@gmail.com>
2020-06-22 17:55:43 -0300

    Done connection with database, create and list

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,19 +1,15 @@
-// Representação de como um dado é salvo na aplicação
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-import { uuid } from 'uuidv4';
-
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
