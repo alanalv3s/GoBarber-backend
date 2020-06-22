33a7435c7c3fd896684e465171c841ace4e0bf75
alanalv3s <alan_ams97@gmail.com>
2020-06-22 14:38:24 -0300

    Init migrations

---
+++ src/database/migrations/1592841279990-CreateAppointments.ts
@@ -0,0 +1,39 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1592841279990
+  implements MigrationInterface {
+  // eslint-disable-next-line class-methods-use-this
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+// Migrations são como o Git faz com o código, é a estrutura padrão do banco
+// em forma de código
