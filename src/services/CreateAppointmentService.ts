bf9931564b0cb3413f5f4b22097a0db2c3880ee2
alanalv3s <alan_ams97@gmail.com>
2020-06-23 14:24:02 -0300

    Created error Class to handle exceptions

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -2,8 +2,10 @@
 /* eslint-disable class-methods-use-this */
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
 
 interface Request {
   provider_id: string;
@@ -21,7 +23,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
