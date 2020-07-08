bd2528c53a068f478f84aaea00b2837f110adfa4
alanalv3s <alan_ams97@gmail.com>
2020-07-07 21:51:26 -0300

    fix: Eslint-error

--- src/shared/infra/typeorm/database/migrations/1592841279990-CreateAppointments.ts
+++ src/shared/infra/typeorm/database/migrations/1592841279990-CreateAppointments.ts
@@ -1,9 +1,7 @@
-/* eslint-disable class-methods-use-this */
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateAppointments1592841279990
   implements MigrationInterface {
-  // eslint-disable-next-line class-methods-use-this
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
